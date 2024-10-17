0e6bd3b1ae9a37f3deb865c9d2643be256bb3774
Elena Rivilla <mariaelenarivilla@paucasesnovescifp.cat>
2024-10-17 18:25:07 +0200

    Añadido JS del piano versión profe

--- PianoVersionProfe/assets/js/funcionalidadPiano.js
+++ PianoVersionProfe/assets/js/funcionalidadPiano.js
@@ -1,49 +1,78 @@
 let cerca = [];
 
-function Nota(nota, tipus){
+function Nota(nota, tipus) {
     this.nota = nota;
     this.tipus = tipus;
 }
 
-function addCerca(nota, tipus, cerca){
+function addCerca(nota, tipus, cerca) {
     cerca.push(new Nota(nota, tipus));
 }
 
-addCerca("do", "", cerca);
-addCerca("re", "", cerca);
-addCerca("mi", "", cerca);
-addCerca("fa", "#", cerca);
-console.log(cerca);
-
-function Partitura(nombre, melodia){
+function Partitura(nombre, melodia) {
     this.nombre = nombre;
     this.melodia = melodia;
 }
 
-melodiaBalanguera = 
-[   new Nota("do", "normal"),
-    new Nota("re", "normal"),
-    new Nota("mi", "normal"),
-    new Nota("fa", "normal"),
-    new Nota("fa", "normal"),
-    new Nota("sol", "normal"),
-    new Nota("sol", "normal"),
-    new Nota("la", "sostenido"),
-    new Nota("la", "sostenido")
-];
+function cercador(partituras, cerca) {
+    let resultat = [];
 
-melodiaHappyBirthday = 
-[
-        new Nota("do", "normal"),
-        new Nota("do", "normal"),
-        new Nota("re", "normal"),
-        new Nota("do", "normal"),
-        new Nota("fa", "normal"),
-        new Nota("mi", "normal"),
-        new Nota("do", "normal"),
-        new Nota("do", "normal"),
-        new Nota("re", "normal"),
-        new Nota("do", "normal"),
-        new Nota("sol", "normal"),
-        new Nota("fa", "normal"),
-]
+    for (let i = 0; i <= partituras.length; i++) {
+        for (let j = 0; j <= (partituras.melodia.length - cerca.length); j++) {
+            let trobat = 1;
+
+            for (let k = 0; k < cerca.length; k++) {
+                if (partituras[i + j + k].melodia.nota !== cerca[k].nota || partituras[i + j + k].melodia.tipus !== cerca[k].tipus) {
+                    break;
+                }
+            }
+            if (trobat) {
+                resultat.push(partituras[i]);
+            }
+        }
+    }
+}
+
+
+let melodiaBalanguera =
+    [new Nota("do", ""),
+    new Nota("re", ""),
+    new Nota("mi", ""),
+    new Nota("fa", ""),
+    new Nota("fa", ""),
+    new Nota("sol", ""),
+    new Nota("sol", ""),
+    new Nota("la", "#"),
+    new Nota("la", "#")
+    ];
+
+let melodiaHappyBirthday =
+    [
+        new Nota("do", ""),
+        new Nota("do", ""),
+        new Nota("re", ""),
+        new Nota("do", ""),
+        new Nota("fa", ""),
+        new Nota("mi", ""),
+        new Nota("do", ""),
+        new Nota("do", ""),
+        new Nota("re", ""),
+        new Nota("do", ""),
+        new Nota("sol", ""),
+        new Nota("fa", ""),
+    ];
+
+let partituraLaBalangera = new Partitura("La Balanguera", melodiaBalanguera);
+let partituraHappyBirthday = new Partitura("Happy Birthday", melodiaHappyBirthday);
+
+let partituras = [partituraLaBalangera, partituraHappyBirthday];
+
+console.log(partituras);
+
+addCerca("do", "", cerca);
+addCerca("do", "", cerca);
+addCerca("re", "", cerca);
+
+
+console.log(cerca);
+console.log(cercador(partituras, cerca)); 
