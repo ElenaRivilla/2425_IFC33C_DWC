8b9949d25246dc2a5dc4a45f1b1b90aefde29ac2
Elena Rivilla <mariaelenarivilla@paucasesnovescifp.cat>
2024-11-25 01:12:53 +0100

    Añadido segundo bloque  act DOM

---
+++ Tema2/DWC_ut3_hej_2_DOM/ejercicio1/js/ejercicio1.js
@@ -0,0 +1,15 @@
+function generarTabla(){
+    let fila = [];
+
+    let resultado = []
+
+    for(let i = 0; i < 11; i++){
+        fila.push(i);
+
+       
+        resultado.push(fila);
+    }
+    
+    console.log(resultado)
+}
+generarTabla();
